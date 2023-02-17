5cf26c94aceccc00cfadb1831de0b852476fa0e1
kuber <kuber@AsurRawan>
2023-02-17 19:09:01 +0530

    testcase2

---
+++ kabil/src/kabilTestes/Tastecases.java
@@ -0,0 +1,14 @@
+package kabilTestes;
+
+public class Tastecases {
+
+	public static void main(String[] args) 
+	{ int class1,class2;
+	class1=30;
+	class2=40;
+	System.out.println(class1+class2);
+		
+
+	}
+
+}
